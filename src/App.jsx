3e9d95c0cc351fbc5f011ea0f80d033ea4c645bc
Eswar <eswar.reddysn@gmail.com>
2025-07-10 16:17:25 +0530

    kk

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // ✅ Context and Utility Components
 import { ThemeProvider } from "./context/ThemeContext";
 import ThemeToggle from "./components/ThemeToggle";
-import ScrollToTopButton from "./components/scrollToTopButton";
+import ScrollToTopButton from "./components/ScrollToTopButton";
 
 // ✅ Layout
 import MainLayout from "./layout/MainLayout";
